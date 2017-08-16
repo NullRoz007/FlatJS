65ee95fb0dd6e15dd4dd96a5526ebbab899e4ae9
NullRoz007 <spencerbog1@gmail.com>
2017-08-16 22:30:48 +1200

    Initial Commit

---
+++ examples/flatexample.js
@@ -0,0 +1,10 @@
+/*
+This code is like, open source or something.
+Do whatever the fuck you wanna do.
+Just include this notice, or don't, whatever.
+
+FLATJS v1.0.2
+*/
+var bungie = (flat require_flat("flatbungie.js")).Data;
+var membership_id = bungie.GetMembershipIdByDisplayName("NullRoz007");
+console.log(`NullRoz007's MembershipId is ${membership_id}`);
---
+++ flat-result.js
@@ -0,0 +1,14 @@
+/*
+This code is like, open source or something.
+Do whatever the fuck you wanna do.
+Just include this notice, or don't, whatever.
+
+FLATJS v1.0.2
+*/
+
+var FlatResult = function(){
+  this.Error = null,
+  this.Data = null
+}
+
+module.exports = FlatResult;
